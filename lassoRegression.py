3d4114b440e36cefeadcb8e2caa0ffc81bd99ba2
Sterling Watson <smwatso1@uno.edu>
2023-08-27 14:20:19 -0500

    adding a SVD recommender

--- lassoRegression.py
+++ lassoRegression.py
@@ -1,48 +1,50 @@
-import pandas
 import numpy
+import pandas
+import matplotlib
 import seaborn
-import matplotlib.pyplot as pyplot
+from matplotlib import pyplot
 from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
-ratings_dataframe = pandas.read_csv("u.data", sep = "\t", names = ["user_id", "movie_id", "rating", "timestamp"], encoding = "ISO-8859-1")
+ratings_dataframe = pandas.read_csv("u.data", sep="\t", names=["user_id", "movie_id", "rating", "timestamp"],
+                                    encoding="ISO-8859-1")
 
-#print(ratings_dataframe)
+movies_dataframe = pandas.read_csv("u.item", sep="|",
+                                   names=["movie_id", "title", "release_date", "video_release_date", "IMDb_URL",
+                                          "unknown",
+                                          "Action", "Adventure", "Animation", "Children's", "Comedy", "Crime",
+                                          "Documentary",
+                                          "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance",
+                                          "Sci-Fi", "Thriller", "War", "Western"],
+                                   encoding="ISO-8859-1")
 
-movies_datafame = pandas.read_csv("u.item", sep= "|", 
-                                  names = ["movie_id", "title", "release_date", "video_release_date", "IMDb_URL","unknown", 
-                                           "Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary",
-                                           "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance",
-                                           "Sci-Fi", "Thriller", "War", "Western"], 
-                                   encoding = "ISO-8859-1")
+users_dataframe = pandas.read_csv("u.user", sep="|", names=["user_id", "age", "gender", "occupation", "zip_code"],
+                                  encoding="ISO-8859-1")
 
-#print(movies_datafame)
-
-users_dataframe = pandas.read_csv("u.user", sep= "|", names = ["user_id", "age", "gender", "occupation", "zip_code"], encoding = "ISO-8859-1")
-
-#print(users_dataframe)
+# print(users_dataframe)
 
 NUMBER_OF_GROUPS = 5
 
-users_dataframe["age_group"] = pandas.qcut(users_dataframe["age"], q = NUMBER_OF_GROUPS, precision=0)
+users_dataframe["age_group"] = pandas.qcut(users_dataframe["age"], q=NUMBER_OF_GROUPS, precision=0)
 
-merged_dataframe = pandas.merge(pandas.merge(ratings_dataframe, users_dataframe[["user_id",       #first merge ratings and users
-                                                                                   "age_group",     #with these columns
-                                                                                   "gender",
-                                                                                   "occupation"]],      
-                                                on = "user_id",     #use user id as primary key
-                                                how = "left"),      #left join
-                                movies_datafame,        #take the prior dateframe and merge with movies
-                                on= "movie_id",          
-                                how= "left")
+merged_dataframe = pandas.merge(
+    pandas.merge(ratings_dataframe, users_dataframe[["user_id",  # first merge ratings and users
+                                                     "age_group",  # with these columns
+                                                     "gender",
+                                                     "occupation"]],
+                 on="user_id",  # use user id as primary key
+                 how="left"),  # left join
+    movies_dataframe,  # take the prior dateframe and merge with movies
+    on="movie_id",
+    how="left")
 
 merged_dataframe.drop(["movie_id", "title", "timestamp", "release_date", "video_release_date", "unknown", "IMDb_URL"],
                       axis=1,
-                      inplace = True)
+                      inplace=True)
 
-#print(merged_dataframe)
+# print(merged_dataframe)
 
 merged_dataframe["age_group"] = pandas.Categorical(merged_dataframe["age_group"])
 merged_dataframe["gender"] = pandas.Categorical(merged_dataframe["gender"])
@@ -52,19 +54,17 @@ age_group_dummies = pandas.get_dummies(merged_dataframe["age_group"])
 gender_dummies = pandas.get_dummies(merged_dataframe["gender"])
 occupation_dummies = pandas.get_dummies(merged_dataframe["occupation"])
 
-
 merged_dataframe = pandas.concat([merged_dataframe,
                                   age_group_dummies,
                                   gender_dummies,
                                   occupation_dummies],
-                                  axis = 1)
+                                 axis=1)
 
-
-merged_dataframe.drop(["age_group",         #we don't need this after doing encoding
+merged_dataframe.drop(["age_group",  # we don't need this after doing encoding
                        "gender",
                        "occupation"],
-                       axis =1,
-                       inplace=True)
+                      axis=1,
+                      inplace=True)
 
 lasso_model = Lasso()
 
@@ -73,16 +73,15 @@ alpha = []
 ALPHA_LENGTH = 7
 NUMBER_OF_FOLDS = 5
 
-
-for i in range (ALPHA_LENGTH):
-    alpha.extend(numpy.arange(10**(i-5), 10**(i-4), 10**(i-5)*2))
+for i in range(ALPHA_LENGTH):
+    alpha.extend(numpy.arange(10 ** (i - 5), 10 ** (i - 4), 10 ** (i - 5) * 2))
 
 params = {"alpha": alpha}
 
-lasso_cross_validation = GridSearchCV(estimator= lasso_model,
-                                      param_grid= params,
+lasso_cross_validation = GridSearchCV(estimator=lasso_model,
+                                      param_grid=params,
                                       scoring="neg_mean_absolute_error",
-                                      cv= NUMBER_OF_FOLDS,
+                                      cv=NUMBER_OF_FOLDS,
                                       return_train_score=True,
                                       verbose=1)
 
@@ -92,17 +91,32 @@ X.columns = X.columns.astype(str)
 
 y = merged_dataframe.rating
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-
-#lasso_cross_validation.best_estimator_
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 best_alpha_value_lasso = 1e-05
 
-best_alpha_lasso_model = Lasso(alpha= best_alpha_value_lasso)
+best_alpha_lasso_model = Lasso(alpha=best_alpha_value_lasso)
 
 best_alpha_lasso_model.fit(X_train, y_train)
 
 lasso_error = mean_squared_error(y_test,
-                   best_alpha_lasso_model.predict(X_test))
+                                 best_alpha_lasso_model.predict(X_test))
 
-print(lasso_error) #1.190668
+print(lasso_error)  # 1.190668
+
+lasso_results_dataframe = pandas.DataFrame({
+    "Features": X_train.columns,
+    "Coefficient": best_alpha_lasso_model.coef_
+})
+
+figure, axes = pyplot.subplots(figsize=[7, 15])
+
+seaborn.barplot(x="Coefficient", y="Features", ax=axes, data=lasso_results_dataframe)
+
+lasso_results_dataframe.sort_values(by="Coefficient",
+                                    inplace=True,
+                                    ascending=False)
+
+lasso_results_dataframe.reset_index(inplace=True, drop=True)  # reseting the index
+
+lasso_results_dataframe = lasso_results_dataframe.iloc[:10]
---
+++ svdMovieRecommender.py
@@ -0,0 +1,66 @@
+import pandas
+import os
+from sklearn.preprocessing import LabelEncoder
+from lenskit.algorithms import funksvd
+from lenskit import crossfold, util
+from lenskit.batch import predict
+from lenskit.metrics.predict import rmse
+
+os.environ["MKL_THREADING_LAYER"] = "TBB"
+
+ratings_dataframe = pandas.read_csv("ratings.csv", sep=",")
+
+ratings_dataframe["userId"] = LabelEncoder().fit_transform(ratings_dataframe["userId"])
+ratings_dataframe["movieId"] = LabelEncoder().fit_transform(ratings_dataframe["movieId"])
+
+dataframe_renamed = ratings_dataframe.rename(columns={
+    "userId": "user",
+    "movieId": "item",
+    "rating": "rating"
+})
+
+NUMBER_OF_FEATURES = 10
+kFolds = 10
+nSamples = 1
+
+svd_model = funksvd.FunkSVD(NUMBER_OF_FEATURES)
+
+
+def calculate_error(model, train_subset, test_subset):
+    svd_model_copy = util.clone(model)
+    svd_model_copy.fit(train_subset)
+
+    predictions = predict(svd_model_copy, test_subset)
+
+    return predictions
+
+
+def train_model(dataframe, model):
+    train_data = []
+    test_data = []
+    subset_errors = []
+
+    partitioned_users = crossfold.partition_users(dataframe, kFolds, crossfold.SampleN(nSamples))
+
+    fold_index = 1
+
+    for train_subset, test_subset in partitioned_users:
+        train_data.append(train_subset)
+        test_data.append(test_subset)
+
+        predictions = calculate_error(model, train_subset, test_subset)
+
+        error = rmse(predictions["prediction"], predictions["rating"])
+
+        subset_errors.append(error)
+
+        fold_index += 1
+
+        print(fold_index)
+
+    return subset_errors
+
+
+error = train_model(dataframe_renamed, svd_model)
+
+print(error)
